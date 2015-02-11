704e3151f85d83ddf8d6524300e649aa70d4a505
Jasper Koid <kslunn@gmail.com>
2015-02-11 15:39:16 +0000

    Added an extra node to publish position vector to IK & cleaned up IK

--- src/ros/kinematics_smart/arm_moveit_generated/src/kinematic_model_tutorial.cpp
+++ src/ros/kinematics_smart/arm_moveit_generated/src/kinematic_model_tutorial.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <iostream>
 #include "std_msgs/Float64.h"
+#include "std_msgs/Float64MultiArray.h"
 // MoveIt!
 #include <moveit/robot_model_loader/robot_model_loader.h>
 #include <moveit/robot_model/robot_model.h>
@@ -39,13 +40,116 @@ std::vector<double> get_kinematic_state(robot_state::RobotStatePtr kinematic_sta
     return joint_values;
 }
 
+bool calcIKSolutionForEffectorPos(Eigen::Vector3d pos, std::vector<double> &output)
+{
+    robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
+    robot_model::RobotModelPtr kinematic_model = robot_model_loader.getModel();
+
+    robot_state::RobotStatePtr kinematic_state(new robot_state::RobotState(kinematic_model));
+    kinematic_state->setToDefaultValues();
+    const robot_state::JointModelGroup* joint_model_group = kinematic_model->getJointModelGroup("Arm");
+   
+    /* Enforce the joint limits for this state and check again*/
+    kinematic_state->enforceBounds();
+    
+    Eigen::VectorXd joints(6);
+    joints << 0, 1.9722, -1.9722, 0, 0, 0;
+
+    //Forward Kinematics
+    kinematic_state->setJointGroupPositions(joint_model_group, joints);
+    Eigen::Affine3d end_effector_state;
+    end_effector_state = kinematic_state->getGlobalLinkTransform("effector_tip_link");
+    
+    Eigen::Vector3d cobra_v;
+
+    //This is the eef position for cobra pose
+    cobra_v << 0.0285793, 0, 0.369884;
+    
+    //this is checked to compile    
+    Eigen::Affine3d frame_transform;
+    frame_transform = kinematic_state->getFrameTransform("effector_tip_link");
+    //The translation is currently according to the frame of the end effector. You need to transform it so that it is based on the model frame. I was thinking of using getFrameTransform.
+    
+    //Convert Affine to Matrix
+    Eigen::Matrix3d frame_transform_Matrix;
+    frame_transform_Matrix = frame_transform.linear();
+    frame_transform_Matrix = frame_transform.matrix().topLeftCorner<3,3>();
+    
+    //Vector to transform eef position
+    Eigen::Vector3d pos_prime;
+    Eigen::Vector3d cobra_v_prime;
+    pos_prime = frame_transform_Matrix.inverse()*pos;
+    cobra_v_prime = frame_transform_Matrix.inverse()*(-cobra_v);
+    
+    //Remove the cobra_pose component so I can specify 3D space directly
+    end_effector_state.translate(cobra_v_prime);
+    end_effector_state.translate(pos_prime);
+    
+    //Inverse Kinematics
+    bool found_ik = kinematic_state->setFromIK(joint_model_group, end_effector_state, 10, 0.1);
+
+    if (found_ik)
+    {
+        output = get_kinematic_state(kinematic_state, joint_model_group);
+    }
+    else
+    {
+        ROS_INFO("Did not find IK solution");
+    }
+
+    return found_ik;
+}
+
+std::vector<ros::Publisher> joint_publishers;
+
+void gotPosition(const std_msgs::Float64MultiArray& msg)
+{
+    // Pull out three components of position into an Eigen vector
+    Eigen::Vector3d pos(msg.data[0], msg.data[1], msg.data[2]);
+    
+    // Run IK
+    std::vector<double> ik_solution;
+    bool found_ik = calcIKSolutionForEffectorPos(pos, ik_solution);
+    
+    // If we found one, publish it.
+    if(found_ik) {
+        for(int i=0; i<5; ++i) {
+            std_msgs::Float64 msg; 
+            msg.data = ik_solution[i];
+            joint_publishers[i].publish(msg);
+        }
+    }
+}
 
 int main(int argc, char **argv)
 {
-    ros::init (argc, argv, "arm_kinematics");
+    ros::init (argc, argv, "arm_ik_solver");
     
     ros::NodeHandle n;
     
+    joint_publishers.push_back(
+        n.advertise<std_msgs::Float64>("shoulder_pan_controller/intermediate_command", 5)
+    );
+    joint_publishers.push_back(
+        n.advertise<std_msgs::Float64>("shoulder_pitch_controller/intermediate_command", 5)
+    );
+    joint_publishers.push_back(
+        n.advertise<std_msgs::Float64>("elbow_flex_controller/intermediate_command", 5)
+    );
+    joint_publishers.push_back(
+        n.advertise<std_msgs::Float64>("wrist_roll_controller/intermediate_command", 5)
+    );
+    joint_publishers.push_back(
+        n.advertise<std_msgs::Float64>("claw_controller/intermediate_command", 5)
+    );
+    
+    ros::Subscriber pos_sub = n.subscribe("pos_for_IK", 5, gotPosition);
+    
+    ros::spin();
+    
+    return 0; // 0 is "success"
+    
+#if 0
     ros::AsyncSpinner spinner(1);
     spinner.start();
 
@@ -97,7 +201,11 @@ int main(int argc, char **argv)
     
     Eigen::Vector3d v;
     Eigen::Vector3d cobra_v;
-    v << 0.1285793, 0, 0.469884;
+    
+    //Listen from pos_for_IK
+    ros::Subscriber sub = n.subscribe("pos_for_IK", 100, getPosition);
+    
+    //v << 0.1285793, 0, 0.469884;
     //This is the eef position for cobra pose
     cobra_v << 0.0285793, 0, 0.369884;
     
@@ -137,7 +245,7 @@ int main(int argc, char **argv)
         std::vector<double> joint_vector = get_kinematic_state(kinematic_state, joint_model_group);
 
         ros::Publisher shoulder_pan_pub = n.advertise<std_msgs::Float64>("shoulder_pan_controller/intermediate_command", 100);
-        ros::Publisher shoulder_pitch_pub = n.advertise<std_msgs::Float64>("shoulder_pitch_controller/intermediate_command", 1000);
+        ros::Publisher shoulder_pitch_pub = n.advertise<std_msgs::Float64>("shoulder_pitch_controller/intermediate_command", 100);
         ros::Publisher elbow_flex_pub = n.advertise<std_msgs::Float64>("elbow_flex_controller/intermediate_command", 100);
         ros::Publisher wrist_roll_pub = n.advertise<std_msgs::Float64>("wrist_roll_controller/intermediate_command", 100);
         ros::Publisher claw_controller_pub = n.advertise<std_msgs::Float64>("claw_controller/intermediate_command", 100);
@@ -183,4 +291,5 @@ int main(int argc, char **argv)
     // END_TUTORIAL
     ros::shutdown();
     return 0;
+#endif
 }
