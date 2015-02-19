e8a0608b2da59454e659e98d81bb19cf85dc0a75
Jasper Koid <kslunn@gmail.com>
2015-02-19 15:35:57 +0000

    Wired IK with 2nd order controller - sending position command every 5 secs

--- src/ros/crustcrawler_smart_arm/smart_arm_controller/scripts/prop_closed_2nd_order_tested.py
+++ src/ros/crustcrawler_smart_arm/smart_arm_controller/scripts/prop_closed_2nd_order_tested.py
@@ -72,17 +72,17 @@ class PropControllerState:
             delta_mag = np.sqrt(np.sum(delta * delta))
             epsilon = 0.05
 
-            #rospy.logerr('p0: '+ str(p0))
-            #rospy.logerr('p1: '+ str(p1))
-            #rospy.logerr('p2: '+ str(p2))
+            rospy.logerr('p0: '+ str(p0))
+            rospy.logerr('p1: '+ str(p1))
+            rospy.logerr('p2: '+ str(p2))
             rospy.logerr('Cmded pose: ' + str(self.commanded_pose))
-            #rospy.logerr('delta: ' +str(delta))
+            rospy.logerr('delta: ' +str(delta))
             #rospy.logerr('Controlling with prev poses: ' + str(self.prev_poses))
 
             # If we're sufficiently close, or have gone mad, transition to listening
             # state and reset the commanded pose.
             if delta_mag <= epsilon or delta_mag >= 3:
-                #rospy.logerr('Finished with delta mag: ' + str(delta_mag))
+                rospy.logerr('Finished with delta mag: ' + str(delta_mag))
                 self.commanded_pose = np.ones(5) * np.nan
                 self.state = PropControllerState.LISTENING
             elif self.pubs is not None:
@@ -100,6 +100,53 @@ class PropControllerState:
 # A global variable holding the controller state
 c_state = PropControllerState()
 
+#    Use rospy.logerr(data.current_pos) to log data
+
+#Initialise, measure in practice
+current_pose = matrix(((-0.9, 0.0, 0.0, 0.0, 0.0),
+                       (-0.9, 0.0, 0.0, 0.0, 0.0),
+                       (-0.9, 0.0, 0.0, 0.0, 0.0)))
+ 
+#last_pose_states = []
+                       
+#goal pose and step size
+#goal pose -- I am not sure why I need 2 lines .. Why delay? The first row is the goal position you want to achieve
+#The second row is just the initial position -- listen in practice
+#joint_commands_goal = matrix(((-0.9, 1.1, -1.25, 0.0, 0.0),
+#0 and 0.0 does not matter
+joint_commands_goal = matrix(((-0.9, 0.0, 0.0, 0.0, 0.0),
+                              (0.0, 0.0, 0.0, 0.0, 0.0)))
+                              
+joint_commands = matrix(((-0.9, 0.0, 0.0, 0.0, 0.0),
+                         (0.0, 0.0, 0.0, 0.0, 0.0)))
+
+"""
+def callback(data):
+    a = data.position
+    
+    current_pose[0,0] = a[3]
+    current_pose[0,1] = a[1]
+    current_pose[0,2] = a[4]
+    current_pose[0,3] = a[2]
+    current_pose[0,4] = a[0]
+    
+    current_pose[1,0] = a[3]
+    current_pose[1,1] = a[1]
+    current_pose[1,2] = a[4]
+    current_pose[1,3] = a[2]
+    current_pose[1,4] = a[0]
+    
+    current_pose[2,0] = a[3]
+    current_pose[2,1] = a[1]
+    current_pose[2,2] = a[4]
+    current_pose[2,3] = a[2]
+    current_pose[2,4] = a[0]
+    
+def listen_callback(event):
+    #pose = current_pose
+    rospy.logerr(current_pose[0])
+"""
+    
 def get_shoulder_pan(event):
     """Called when a new command is sent to the arm."""
     c_state.commanded_pose[0] = event.data
@@ -162,3 +209,77 @@ if __name__ == '__main__':
     while not rospy.is_shutdown():
         c_state.tick()
         r.sleep()
+
+if __name__ == 'we are not called this':
+    pubs = [rospy.Publisher(name + '/command', Float64) for name in joint_names]
+    rospy.init_node('make_goal_pose', anonymous=True)
+        
+    #rospy.sleep(4)
+    
+    rospy.Subscriber('shoulder_pan_controller/intermediate_command', Float64, get_shoulder_pan)   
+    rospy.Subscriber('shoulder_pitch_controller/intermediate_command', Float64, get_shoulder_pitch)
+    rospy.Subscriber('elbow_flex_controller/intermediate_command', Float64, get_elbow_flex)
+    rospy.Subscriber('wrist_roll_controller/intermediate_command', Float64, get_wrist_roll)    
+    rospy.Subscriber('claw_controller/intermediate_command', Float64, get_claw)
+    
+    rospy.Subscriber('shoulder_pan_controller/state', JointState, current_shoulder_pan)   
+    rospy.Subscriber('shoulder_pitch_controller/state', JointState, current_shoulder_pitch)
+    rospy.Subscriber('elbow_flex_controller/state', JointState, current_elbow_flex)
+    rospy.Subscriber('wrist_roll_controller/state', JointState, current_wrist_roll)    
+    rospy.Subscriber('claw_controller/state', JointState, current_claw)
+    
+    #need to subscribe to actual state of the robot, and think of way of implementing the 2nd order controller. How frequent do you want to listen to the states, how frequent do you publish it to the controller? Should I wait for the controller to reach its actual position first??
+    
+    #rospy.sleep(2) #my hypothesis is that the matrix has not been updated yet, hence need time delay
+    
+    #joint_commands_goal must come after current_pose
+    pose = current_pose
+    joint_commands_goal = joint_commands
+    rospy.logerr(joint_commands_goal[0])
+    rospy.logerr("Above is joint commands")
+    joint_commands_goal[1] = pose[0]
+    
+    #for i in range(len(pubs)):
+    #    pubs[i].publish(pose[0,i])
+    
+    #Control the robot to the goal position
+    r = rospy.Rate(30)
+    
+    f = 0
+        
+    #pose = current_pose
+    rospy.logerr(pose[0])
+    rospy.logerr("BOO")
+    
+    #rospy.sleep(2) #wait for first intermediate command
+    
+    while not rospy.is_shutdown():
+
+        #Break or stop the command when the current pose is sufficiently close to the desired pose
+        delta = joint_commands_goal[0] - pose[0]
+        delta_mag = np.linalg.norm(delta)
+        #rospy.logerr(delta_mag) about 2.4
+        epsilon = 0.05
+
+        if f>300 and delta_mag <= epsilon or delta_mag >= 100:
+            break
+            
+        #Second order controller
+        else:
+            pose[2] = pose[1]
+            pose[1] = pose[0]
+            joint_commands_goal[1] = joint_commands_goal[0]
+        
+            pose[0] = (1+alpha-k)*pose[1] - alpha*pose[2] + k*joint_commands_goal[1]
+            rospy.logerr(pose[0])
+            
+            f += 1
+            
+            for i in range(len(pubs)):
+                pubs[i].publish(pose[0,i])
+            
+        r.sleep()
+    
+    #rospy.spin()   
+    #rospy.spin()
+    sys.exit(0)
