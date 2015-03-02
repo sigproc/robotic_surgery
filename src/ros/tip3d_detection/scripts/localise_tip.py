6ad60f8a2b8d54fe890f9d0bcae06bfea1e30907
DuongLe <zuzia478@gmail.com>
2015-03-02 12:06:43 +0000

    localise_tip.py: register with ROS

--- src/ros/tip3d_detection/scripts/localise_tip.py
+++ src/ros/tip3d_detection/scripts/localise_tip.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 
+import rospy
+
 def main():
-    pass
+    # Register the node with ROS
+    rospy.init_node('localise_tip', anonymous=True)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
 
 if __name__=='__main__':
     main()
