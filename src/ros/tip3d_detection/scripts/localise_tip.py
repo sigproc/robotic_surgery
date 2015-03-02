81d2bc6306a464a82d6b3cd8f513f53b4d8339e5
DuongLe <zuzia478@gmail.com>
2015-03-02 13:20:40 +0000

    Added a tip message that contains x, y, z coordinates of a point in space
    Added a publisher to publish x, y, z coordinates to other nodes

--- src/ros/tip3d_detection/scripts/localise_tip.py
+++ src/ros/tip3d_detection/scripts/localise_tip.py
@@ -2,14 +2,31 @@
 
 import rospy
 from sensor_msgs.msg import Image
+from tip3d_detection_msgs.msg import Tip
 from tip3d_detection import image_to_array
 
-STATE={"left":None,"right":None}
+import numpy as np
+
+STATE={"left":None,"right":None,"tip_publisher":None,"last_image_header":None}
 
 def handle_images():
-    rospy.loginfo("Left: %s, Right: %s",STATE["left"].shape, STATE["right"].shape)
+    tip_msg = Tip()
+
+    # Set "header" for tip msg
+    tip_msg.header = STATE["last_image_header"]
+    
+    # TODO: process STATE["left"] and STATE["right"] and get actual tip location
+    
+    # Set position to fake values
+    tip_msg.x = STATE["left"].shape[0]
+    tip_msg.y = STATE["right"].shape[0]
+    tip_msg.z = np.random.random()
+
+    # Publish the tip message to the other nodes which are interested
+    STATE["tip_publisher"].publish(tip_msg)
 
 def got_image(side, image_message):
+    STATE["last_image_header"]=image_message.header
     STATE[side]=image_to_array(image_message)
     if STATE["left"] is not None and STATE["right"] is not None:
         handle_images()
@@ -27,6 +44,9 @@ def main():
     # Subscribe to left and right images
     rospy.Subscriber("left_image", Image, got_left_image)
     rospy.Subscriber("right_image", Image, got_right_image)
+    
+    # Create a publisher for tip locations
+    STATE["tip_publisher"] = rospy.Publisher(rospy.get_name() + '/tip', Tip, queue_size=1)
     
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
