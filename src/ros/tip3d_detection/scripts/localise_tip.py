04921b419363767fa6bde89900709940fef91ddb
DuongLe <zuzia478@gmail.com>
2015-03-02 12:43:09 +0000

    Added an image handler.
    It is called when an image_message is received for both left and right

--- src/ros/tip3d_detection/scripts/localise_tip.py
+++ src/ros/tip3d_detection/scripts/localise_tip.py
@@ -3,12 +3,21 @@
 import rospy
 from sensor_msgs.msg import Image
 
+STATE={"left":None,"right":None}
+
+def handle_images():
+    rospy.loginfo("Left: %s, Right: %s",STATE["left"].width, STATE["right"].width)
+
 def got_left_image(image_message):
-    rospy.loginfo('got left image with width=%s and height=%s', image_message.width, image_message.height)
+    STATE["left"]=image_message
+    if STATE["left"] is not None and STATE["right"] is not None:
+        handle_images()  
     
 def got_right_image(image_message):
-    rospy.loginfo('got right image with width=%s and height=%s', image_message.width, image_message.height)
-
+    STATE["right"]=image_message
+    if STATE["left"] is not None and STATE["right"] is not None:
+        handle_images()
+    
 def main():
     # Register the node with ROS
     rospy.init_node('localise_tip', anonymous=True)
